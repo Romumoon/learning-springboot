5f5176dd5d6c7c2bdc0231c50fcee6669a44741a
Romumoon <romu.pastore@gmail.com>
2022-03-14 14:26:59 -0300

    Order, Instant, ISO 8601 v2

---
+++ src/main/java/com/romumu/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.romumu.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.romumu.course.entities.Order;
+import com.romumu.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
