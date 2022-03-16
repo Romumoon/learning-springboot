1206718f9e2725cea87ccaa9ab9d221cd89d14f4
Romumoon <romu.pastore@gmail.com>
2022-03-16 17:06:29 -0300

    Payment, one-to-one association

--- src/main/java/com/romumu/course/config/TestConfig.java
+++ src/main/java/com/romumu/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.romumu.course.entities.Category;
 import com.romumu.course.entities.Order;
 import com.romumu.course.entities.OrderItem;
+import com.romumu.course.entities.Payment;
 import com.romumu.course.entities.Product;
 import com.romumu.course.entities.User;
 import com.romumu.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
